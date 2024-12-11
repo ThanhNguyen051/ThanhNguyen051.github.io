215de256733142166e18448b1d3685e12cb19356
ThanhNguyen051 <huynhthanhnguyenct2001@gmail.com>
2024-12-11 20:09:12 +0700

    .

--- assets/js/bootsnav.js
+++ assets/js/bootsnav.js
@@ -533,7 +533,7 @@
                     // Scroll
                     var scrollTop = $(window).scrollTop(),
                         $anchor = $(this).find('a'),
-                        $section = $($anchor.attr('href')).offset().top,
+                        $section = $($anchor.attr('href')).offset().top-16500,
                         $window = $(window).width(),
                         $minusDesktop = getNav.data("minus-value-desktop"),
                         $minusMobile = getNav.data("minus-value-mobile"),
