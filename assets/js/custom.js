d2377cb1622ac4d4245581f5b59358dfcb2f5527
ThanhNguyen051 <huynhthanhnguyenct2001@gmail.com>
2024-12-12 16:26:07 +0700

    .

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,17 +19,17 @@ $(document).ready(function(){
 
     // 1. Scroll To Top 
 		$(window).on('scroll',function () {
-			if ($(this).scrollTop() > 600) {
-				$('.return-to-top').fadeIn();
-			} else {
-				$('.return-to-top').fadeOut();
-			}
+			// if ($(this).scrollTop() > 600) {
+			// 	$('.return-to-top').fadeIn();
+			// } else {
+			// 	$('.return-to-top').fadeOut();
+			// }
 		});
 		$('.return-to-top').on('click',function(){
-				$('html, body').animate({
-				scrollTop: 0
-			}, 1500);
-			return false;
+			// 	$('html, body').animate({
+			// 	scrollTop: 0
+			// }, 1500);
+			// return false;
 		});
 	
 	
@@ -37,22 +37,22 @@ $(document).ready(function(){
 	// 2. Smooth Scroll spy
 		
 		$('.header-area').sticky({
-           topSpacing:0
+        //    topSpacing:0
         });
 		
 		//=============
 
 		$('li.smooth-menu a').bind("click", function(event) {
-			event.preventDefault();
-			var anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - 0
-			}, 1200,'easeInOutExpo');
+			// event.preventDefault();
+			// var anchor = $(this);
+			// $('html, body').stop().animate({
+			// 	scrollTop: $(anchor.attr('href')).offset().top - 0
+			// }, 1200,'easeInOutExpo');
 		});
 		
 		$('body').scrollspy({
-			target:'.navbar-collapse',
-			offset:0
+			// target:'.navbar-collapse',
+			// offset:0
 		});
 
 	// 3. Progress-bar
@@ -61,13 +61,13 @@ $(document).ready(function(){
 		var progressBar = $(".progress-bar");
 		if (progressBar.length) {
 			progressBar.appear(function () {
-				dataToggleTooTip.tooltip({
-					trigger: 'manual'
-				}).tooltip('show');
-				progressBar.each(function () {
-					var each_bar_width = $(this).attr('aria-valuenow');
-					$(this).width(each_bar_width + '%');
-				});
+				// dataToggleTooTip.tooltip({
+				// 	trigger: 'manual'
+				// }).tooltip('show');
+				// progressBar.each(function () {
+				// 	var each_bar_width = $(this).attr('aria-valuenow');
+				// 	$(this).width(each_bar_width + '%');
+				// });
 			});
 		}
 	
@@ -114,13 +114,13 @@ $(document).ready(function(){
     // 5. welcome animation support
 
         $(window).load(function(){
-        	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
+        	// $(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
+            // $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
         });
 
         $(window).load(function(){
-        	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
-            $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
+        	// $(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
+            // $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
         });
 
 });
