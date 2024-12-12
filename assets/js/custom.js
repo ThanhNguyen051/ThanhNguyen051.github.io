5b745705ca7b53caf540460f9142894648cd1d3e
ThanhNguyen051 <huynhthanhnguyenct2001@gmail.com>
2024-12-12 19:46:01 +0700

    .

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,17 +19,17 @@ $(document).ready(function(){
 
     // 1. Scroll To Top 
 		$(window).on('scroll',function () {
-			// if ($(this).scrollTop() > 600) {
-			// 	$('.return-to-top').fadeIn();
-			// } else {
-			// 	$('.return-to-top').fadeOut();
-			// }
+			if ($(this).scrollTop() > 600) {
+				$('.return-to-top').fadeIn();
+			} else {
+				$('.return-to-top').fadeOut();
+			}
 		});
 		$('.return-to-top').on('click',function(){
-			// 	$('html, body').animate({
-			// 	scrollTop: 0
-			// }, 1500);
-			// return false;
+				$('html, body').animate({
+				scrollTop: 0
+			}, 1500);
+			return false;
 		});
 	
 	
