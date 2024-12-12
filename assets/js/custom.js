a1598aa628f6b268f2eb2d329de602bfb90b1701
ThanhNguyen051 <huynhthanhnguyenct2001@gmail.com>
2024-12-12 20:29:08 +0700

    .

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
 			var anchor = $(this);
 			$('html, body').stop().animate({
 				scrollTop: $(anchor.attr('href')).offset().top
-			}, 1200,'easeInOutExpo');
+			}, 1,'easeInOutExpo');
 		});
 		
 		$('body').scrollspy({
@@ -61,13 +61,13 @@ $(document).ready(function(){
 		var progressBar = $(".progress-bar");
 		if (progressBar.length) {
 			progressBar.appear(function () {
-				// dataToggleTooTip.tooltip({
-				// 	trigger: 'manual'
-				// }).tooltip('show');
-				// progressBar.each(function () {
-				// 	var each_bar_width = $(this).attr('aria-valuenow');
-				// 	$(this).width(each_bar_width + '%');
-				// });
+				dataToggleTooTip.tooltip({
+					trigger: 'manual'
+				}).tooltip('show');
+				progressBar.each(function () {
+					var each_bar_width = $(this).attr('aria-valuenow');
+					$(this).width(each_bar_width + '%');
+				});
 			});
 		}
 	
@@ -76,51 +76,51 @@ $(document).ready(function(){
 		// i. client (carousel)
 		
 			$('#client').owlCarousel({
-				// items:7,
-				// loop:true,
-				// smartSpeed: 1000,
-				// autoplay:true,
-				// dots:false,
-				// autoplayHoverPause:true,
-				// responsive:{
-				// 		0:{
-				// 			items:2
-				// 		},
-				// 		415:{
-				// 			items:2
-				// 		},
-				// 		600:{
-				// 			items:4
+				items:7,
+				loop:true,
+				smartSpeed: 1000,
+				autoplay:true,
+				dots:false,
+				autoplayHoverPause:true,
+				responsive:{
+						0:{
+							items:2
+						},
+						415:{
+							items:2
+						},
+						600:{
+							items:4
 
-				// 		},
-				// 		1199:{
-				// 			items:4
-				// 		},
-				// 		1200:{
-				// 			items:7
-				// 		}
-				// 	}
+						},
+						1199:{
+							items:4
+						},
+						1200:{
+							items:7
+						}
+					}
 				});
 				
 				
-				// $('.play').on('click',function(){
-				// 	owl.trigger('play.owl.autoplay',[1000])
-				// })
-				// $('.stop').on('click',function(){
-				// 	owl.trigger('stop.owl.autoplay')
-				// })
+				$('.play').on('click',function(){
+					owl.trigger('play.owl.autoplay',[1000])
+				})
+				$('.stop').on('click',function(){
+					owl.trigger('stop.owl.autoplay')
+				})
 
 
     // 5. welcome animation support
 
         $(window).load(function(){
-        	// $(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            // $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
+        	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
+            $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
         });
 
         $(window).load(function(){
-        	// $(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
-            // $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
+        	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
+            $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
         });
 
 });
